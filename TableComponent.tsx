d588658d6efec9f1922a037af89f9c7a59d6a0d9
MunnangiSrikanya <109782835+MunnangiSrikanya@users.noreply.github.com>
2024-09-19 14:02:43 +0530

    Update TableComponent.tsx

--- TableComponent.tsx
+++ TableComponent.tsx
@@ -1,34 +1,93 @@
-/* eslint-disable */
-import { TableBody, TableCell, TableContainer, TableHead, TableRow, Table } from '@material-ui/core';
 import React from 'react';
+import {
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Table,
+  Button,
+  IconButton,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete'
 
-// Define a type for the column prop
+// Define the type for a column
 interface Column {
+  headerName:string;
   field: string;
 }
-
+interface options {
+  [key: string]: string;
+  carrierName: string;
+  code: string;
+  lineofBusiness: string;
+}
+// Define the type for props
 interface TableComponentProps {
   columns: Column[];
+  rowData: options[];
+  selectedFileType:string;
+  handleDelete:(index: number) => void;
 }
 
-const TableComponent: React.FC<TableComponentProps> = ({ columns }) => {
+// Functional component with TypeScript
+const TableComponent: React.FC<TableComponentProps> = ({ columns, rowData ,selectedFileType,handleDelete}) => {
+  console.log(rowData)
   return (
-    <div>
+    <div style={{ padding: '12px' }}>
       <TableContainer style={{ border: '1px solid lightGray' }}>
-        <Table aria-label='simple table'>
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              {columns.map((column, index) => (
-                <TableCell key={index} style={{ borderRight: '1px solid lightGray' }}>
-                  {column.field}
+              {columns?.map((column, index) => (
+                <TableCell key={index} style={{ textAlign: 'center' }}>
+                  {column.headerName}
                 </TableCell>
               ))}
+              <TableCell></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell align='right'>No items for the filters applied</TableCell>
-            </TableRow>
+            {rowData?.length === 0 ? (
+              <TableRow>
+                <TableCell
+                  colSpan={columns?.length+1}
+                  style={{ textAlign: 'center' }}
+                >
+                  No data
+                </TableCell>
+
+              </TableRow>
+            ) : (
+              rowData?.map((row, index) => (
+                <TableRow key={index}>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.field}
+                      style={{  textAlign: 'center' }}
+                    >
+                      {row[column.field]}
+                    </TableCell>
+                  ))}
+                   <TableCell style={{ textAlign: 'center' }}>
+                      <IconButton onClick={() => handleDelete(index)}>
+                        <DeleteIcon/>
+                      </IconButton>
+                    </TableCell>
+                </TableRow>
+              ))
+            )}
+            {selectedFileType!=='PDL'&& <TableRow>
+              <TableCell colSpan={columns.length+1} style={{ border: '1px solid lightGray' }}>
+                +
+                <Button
+                  style={{ color: '#035C67', textDecoration: 'underline', textTransform: 'none' }}
+                >
+                  {' '}
+                  <b>Add New Row</b>
+                </Button>
+              </TableCell>
+            </TableRow>}
           </TableBody>
         </Table>
       </TableContainer>
